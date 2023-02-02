059bdacfcc034d231a03b21d3fe906b8e546bbda
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-02-02 10:43:12 +0500

    HI

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
-    }
-}
+        //Implement the program that will print the season name to the console by the number of the month.
+        //If month is incorrect -> print "Wrong month number". Month will be passed as the method argument.
+        //(Example: "1" -> "Winter", 0 -> "Wrong month number")
+        switch (monthNumber) {
+            case 12:
+                System.out.println("Winter");break;
+            case 1:
+                System.out.println("Winter");break;
+            case 2:
+                System.out.println("Winter");break;
+            case 3:
+                System.out.println("Spring");break;
+            case 4:
+                System.out.println("Spring");break;
+            case 5:
+                System.out.println("Spring");break;
+            case 6:
+                System.out.println("Summer");break;
+            case 7:
+                System.out.println("Summer");break;
+            case 8:
+                System.out.println("Summer");break;
+            case 9:
+                System.out.println("Autumn");break;
+            case 10:
+                System.out.println("Autumn");break;
+            case 11:
+                System.out.println("Autumn");break;
+            default:
+                System.out.println("Wrong month number"); break;
+
+
+        }
+
+    }}
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,9 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
-    public void validate(double firstSide, double secondSide, double thirdSide) {
+    public void validate(double a, double b, double c) {
+ if (a + b <= c || a + c <= b || b + c <= a) System.out.println("it's not a triangle");
+ else System.out.println("this is a valid triangle");
 
     }
 }
